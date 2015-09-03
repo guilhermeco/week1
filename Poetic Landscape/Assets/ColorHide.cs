d7a24bb3b08f7f794e260237d0641dbe4f692c7f
Guilherme Coelho <guilhermeismco@gmail.com>
2015-09-03 17:59:36 -0400

    week 1 day 2

---
+++ Poetic Landscape/Assets/ColorHide.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ColorHide : MonoBehaviour {
+	
+
+	void OnTriggerEnter() {
+			gameObject.GetComponent<MeshRenderer>().material.color = Color.blue;
+
+			GameObject.Find ("BigCube").SetActive(false);
+
+	}
+}
---
+++ Poetic Landscape/Assets/LightTrigger.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LightTrigger : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	//when the player walks into the trigger on this gameobject
+	void OnTriggerEnter(Collider other) {
+		Debug.Log ("The Trigger is working");
+		//get the light component on this gameobject, turn it off
+		gameObject.GetComponent<Light>().enabled = false; 
+	}
+
+	void OnTriggerExit(Collider other) {
+		Debug.Log ("Trigger exit is working");
+		gameObject.GetComponent<Light>().enabled = true;
+	}
+
+}
